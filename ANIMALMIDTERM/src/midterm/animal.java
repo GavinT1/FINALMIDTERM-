ac350d70bdd59c758bc4f2b11a2203f9140f2442
Gavin Tung <gavin.tung@ciit.edu.ph>
2025-02-20 23:46:25 +0800

    Add files via upload

--- ANIMALMIDTERM/src/midterm/animal.java
+++ ANIMALMIDTERM/src/midterm/animal.java
@@ -1,16 +1,18 @@
 package midterm;
 
+import java.util.Scanner;
+
 class Dog {
     private String dogname;
     private String breed;
 
-  
+
     public Dog(String name, String breed) {
         this.dogname = name;
         this.breed = breed;
     }
 
-  
+
     public void setName(String name) {
         this.dogname = name;
     }
@@ -25,24 +27,40 @@ class Dog {
     }
 
     public static void main(String[] args) {
-     
-        Dog d1 = new Dog("Bea", "Chuwawa");
-        Dog d2 = new Dog("Jucky", "husky");
+        Scanner scanner = new Scanner(System.in);
 
- 
-        System.out.println("Before:");
+        System.out.print(" name of the first dog: ");
+        String name1 = scanner.nextLine();
+        System.out.print(" breed of the first dog: ");
+        String breed1 = scanner.nextLine();
+
+        System.out.print(" name of the second dog: ");
+        String name2 = scanner.nextLine();
+        System.out.print(" breed of the second dog: ");
+        String breed2 = scanner.nextLine();
+
+        Dog d1 = new Dog(name1, breed1);
+        Dog d2 = new Dog(name2, breed2);
+
+        System.out.println("\nBefore:");
         d1.displayDog();
         d2.displayDog();
 
-  
-        d1.setName("Rocky");
-        d1.setBreed("bulldog");
-        d2.setName("Gavin");
-        d2.setBreed("Dachshund");
+        System.out.print("\n new name for the first dog: ");
+        d1.setName(scanner.nextLine());
+        System.out.print("Enter the new breed for the first dog: ");
+        d1.setBreed(scanner.nextLine());
 
-    
+        System.out.print(" new name for the second dog: ");
+        d2.setName(scanner.nextLine());
+        System.out.print(" new breed for the second dog: ");
+        d2.setBreed(scanner.nextLine());
+
+
         System.out.println("\nAfter:");
         d1.displayDog();
         d2.displayDog();
+
+        scanner.close();
     }
 }
